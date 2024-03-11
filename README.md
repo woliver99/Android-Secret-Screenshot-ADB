765e44738a289c23abd5a2fd476c653118af73bc
woliver99 <oliver@maplenetwork.ca>
2024-03-11 16:21:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Android-Screenshot-ADB
+
+Allows you to take a screenshot of your Android screen without any apps including snapchat being notified.
+It works by using usb debugging and a windows pc so you dont need root on your device.
+
+# Enable USB Debugging
+
+1.  Open settings and open the "About phone" category
+2.  Find "Build number"
+3.  Spam tab "Build number" until developper mode is enabled
+4.  Once developer modes enabled go to the "System" category in settings and open "Developer options"
+5.  Find the option for "USB debugging" and enable it
+
+# Running the program
+
+1.  Download the exe from the latest releases
+2.  Run the exe
+3.  If prompted allow USB debugging on your phone
+4.  Screenshot should be taken and open on your pc
---
+++ run/Run.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcAuONX2/OpEZ++Pv4Pq7lkgQRKITd4zazr2BLtw971Kkc5kh0n9I2INeRSdKaxyXfgM1vWhNpGnII8iTvxyB
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBRYRRKHAE+/Fb4I5/jH2++Ksg0xW+E+bIbU1IigJfdT7ErocZM/ljII0fctAh9KcRuvUTw3u2tHuHeNONOjhwryXgaM/k5Q
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+platform-tools\adb exec-out screencap -p > "screen.png"
+platform-tools\adb kill-server
+screen.png
